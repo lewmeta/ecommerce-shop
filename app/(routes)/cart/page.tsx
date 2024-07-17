47e403d4ffcbda7b4919b9cee7d3c8f4dc42b003
thegypsyboyo <lewisonyango9@gmail.com>
2024-07-17 19:37:43 +0300

    made changes to cart

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,15 +1,17 @@
-"use client"
+"use client";
 
-import Container from "@/components/ui/container";
-import useCart from "@/hooks/use-store";
-import { useEffect, useState } from "react";
-import CartItem from "./components/cart-item";
-import Summery from "./components/summery";
+import { useEffect, useState } from 'react';
 
+import Container from '@/components/ui/container';
+import useCart from '@/hooks/use-cart';
+
+import Summary from './components/summary'
+import CartItem from './components/cart-item';
+
+export const revalidate = 0;
 
 const CartPage = () => {
-    const [isMounted, setIsMounted] = useState<boolean>(false);
-
+    const [isMounted, setIsMounted] = useState(false);
     const cart = useCart();
 
     useEffect(() => {
@@ -23,26 +25,23 @@ const CartPage = () => {
     return (
         <div className="bg-white">
             <Container>
-                <div className="px-4 py-16 lg:px-8">
-                    <h1 className="text-3xl font-bold text-black">Shopping cart</h1>
+                <div className="px-4 py-16 sm:px-6 lg:px-8">
+                    <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
                     <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
                         <div className="lg:col-span-7">
-                            {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart</p>}
+                            {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
                             <ul>
                                 {cart.items.map((item) => (
-                                    <CartItem
-                                        key={item.id}
-                                        data={item}
-                                    />
+                                    <CartItem key={item.id} data={item} />
                                 ))}
                             </ul>
                         </div>
-                        <Summery />
+                        <Summary />
                     </div>
                 </div>
             </Container>
         </div>
     )
-}
+};
 
 export default CartPage;
