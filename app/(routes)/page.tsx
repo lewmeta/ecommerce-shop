00ef18f8e64ad1eaec35a2d376ae0107311f5a1e
thegypsyboyo <lewisonyango9@gmail.com>
2024-07-17 20:49:47 +0300

    something here

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -7,16 +7,16 @@ import Container from "@/components/ui/container";
 // export const revalidate = 0;
 
 const HomePage = async () => {
-    // const products = await getProducts({ isFeatured: true })
-    // const billboard = await getBillboard("4adb0235-01c2-48a3-9130-337bfa53f9f9")
+    const products = await getProducts({ isFeatured: true })
+    const billboard = await getBillboard("4adb0235-01c2-48a3-9130-337bfa53f9f9")
     return (
         <Container>
             <div className="space-y-10 pb-10">
-                {/* <Billboard data={billboard} />
+                <Billboard data={billboard} />
 
                 <div className="flx flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
                     <ProductList title="Featured Products" items={products} />
-                </div> */}
+                </div>
                 page
 
             </div>
